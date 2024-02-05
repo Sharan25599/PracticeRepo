041fa10fe39f02d2c3c68f456819a971dee95cf8
SharanKumar <sharankumar.d@diggibyte.com>
2024-02-05 18:38:11 +0530

    practiced on math_func(Sqrt,pow,floor,ceil,pi)

---
+++ import_math_func.py
@@ -0,0 +1,31 @@
+import math
+
+x=math.sqrt(25)
+print(x)
+
+x=math.sqrt(15)
+print(x)
+
+x=math.floor(2.3)
+print(x)
+
+x=math.ceil(2.3)
+print(x)
+
+x=math.floor(5.8)
+print(x)
+
+x=math.ceil(5.8)
+print(x)
+
+a=math.pow(4,2)
+print(a)
+
+a=math.pi
+print(a)
+
+from math import sqrt,pow
+
+print(pow(4,2))
+
+print(sqrt(49))
